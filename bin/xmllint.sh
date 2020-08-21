a9d2d89e5091e103c826b797795bce9dc8248b52
Viktor Szépe <viktor@szepe.net>
2020-08-21 04:37:38 +0200

    ci: use xmllint to validate the ruleset (#3)
    * Check ruleset
    * Create xmllint.sh
    * Update .gitignore
    * Enalbe executable bit of xmllint
    * Update xmllint.sh
    * Fix XML

---
+++ bin/xmllint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Lint ruleset XML file.
+#
+# BASH-VERSION  :4.2+
+# DEPENDS       :apt-get install wget libxml2-utils
+
+RULESET="src/Ramsey/ruleset.xml"
+
+set -e
+
+# Current directory should be repository root
+test -r "$RULESET"
+
+# Check dependency
+hash xmllint
+
+# Create temporary directory
+mkdir -p tmp
+
+# Download XML schema definitions
+wget -nv -N -P tmp/ "https://github.com/squizlabs/PHP_CodeSniffer/raw/master/phpcs.xsd"
+wget -nv -N -P tmp/ "https://www.w3.org/2012/04/XMLSchema.xsd"
+
+xmllint --noout --schema tmp/XMLSchema.xsd tmp/phpcs.xsd
+xmllint --noout --schema tmp/phpcs.xsd "$RULESET"
+diff -B "$RULESET" <(XMLLINT_INDENT="    " xmllint --format "$RULESET")
